3e7713c80a74d4a4b5deca814afb50e0f1907814
italo <italobeckmann@gmail.com>
2025-09-26 18:04:53 -0300

    Adicionar arquivos de projeto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace OrusFinancas.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
